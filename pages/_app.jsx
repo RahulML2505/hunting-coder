bfec0e4c86df64fa8d03807d7ef9b3c41c392b8c
RahulML2505 <rahulml2505ytc@gmail.com>
2022-02-22 22:39:49 +0530

    CWH - Video_13: Made Some Cleanup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css';
-// import '../styles/styles.css';
-// import Dummy from '../components/dummy';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Dummy /> */}
       <Component {...pageProps} />
     </>
   );
